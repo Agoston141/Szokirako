bb1ebe4b1ebd73f4fd90256a9e253a55b72dfb6f
Agoston141 <molnar.agoston2006@gmail.com>
2023-12-07 20:45:39 +0100

    Basic ablak

--- stokirako.py
+++ stokirako.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("300x400")
+root.geometry("400x500")
 root.title("Szókirakó")
+lable =tk.Label(root, text="szókirakó játék", font=('Calibri',18))
+lable.pack(padx=20,pady=20)
+textBox= tk.Entry(root,)
+textBox.pack()
+Button=tk.Button(root,text="Ellenőrzés",font=('Calibri',13))
+Button.pack(padx=10,pady=10)
+
+
 root.mainloop()
